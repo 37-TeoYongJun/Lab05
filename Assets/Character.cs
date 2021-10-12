50243636bd5ed49bc8c505d67dc2d068b46bbd17
37-TeoYongJun <yongjun447@gmail.com>
2021-10-12 22:12:41 +0800

    collide wif coin add point n update score ui
    yay

---
+++ Assets/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    private float score;
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            score += 10;
+            scoreText.text = "Score: " + score;
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
