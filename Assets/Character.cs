f63b47908ef2d3674d54a8896acb63f09ec0d316
37-TeoYongJun <yongjun447@gmail.com>
2021-10-12 23:33:36 +0800

    add win lose condition n audio n update gitignore
    finish liao jn forget update more

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -6,9 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
+    public float totalCoins;
+    public float timeLeft;
+    public float timeRemaining;
+
     private float score;
+    private float timeValue;
 
     public Text scoreText;
+    public Text timeText;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +25,34 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+
+        timeRemaining = Mathf.FloorToInt(timeLeft % 60);
+
+        timeText.text = "Time: " + timeRemaining.ToString();
+
+        if (score >= totalCoins)
+        {
+            if (timeLeft >= timeValue)
+            {
+                SceneManager.LoadScene("GameWinScene");
+            }
+        }
+
+        else if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
     {
+        ParticleSystem collect = GameObject.Find("CollectParticle").GetComponent<ParticleSystem>();
+
         if (collision.gameObject.tag == "Coin")
         {
+            collect.Play();
+
             score += 10;
             scoreText.text = "Score: " + score;
 
